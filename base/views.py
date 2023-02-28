8e31c208501b35159fce90d664d7a553d53c0e7e
muhameddadel <102751203+muhameddadel@users.noreply.github.com>
2023-02-28 08:07:51 +0200

    TaskList model and class based view

---
+++ base/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Task
+# Create your views here.
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
